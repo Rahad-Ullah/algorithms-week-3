977e7e75214e01dd3be0326e699b22342d02d62c
Rahad Ullah <rahadullah10@gmail.com>
2026-05-20 22:49:46 +0600

    solve: problem-2

---
+++ Shortest_Distance.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long INF = LLONG_MAX;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    long long adj_matrix[n + 1][n + 1];
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == j)
+                adj_matrix[i][j] = 0;
+            else
+                adj_matrix[i][j] = INF;
+        }
+    }
+
+    while (e--)
+    {
+        int a, b;
+        long long c;
+        cin >> a >> b >> c;
+        adj_matrix[a][b] = min(adj_matrix[a][b], c);
+    }
+
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (adj_matrix[i][k] != INF && adj_matrix[k][j] != INF)
+                {
+                    if (adj_matrix[i][k] + adj_matrix[k][j] < adj_matrix[i][j])
+                    {
+                        adj_matrix[i][j] = adj_matrix[i][k] + adj_matrix[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (adj_matrix[a][b] == INF)
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            cout << adj_matrix[a][b] << endl;
+        }
+    }
+
+    return 0;
+}
